273ea8bd4ebbcf1a039600b35e4d616c7baab592
Thanh <mtthanh1986@gmail.com>
2012-11-27 11:02:06 +0700

    build project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostEcpal::Application.config.secret_token = '155ab6f2006c6f2f5d81f3892c0f0d7404aa864e6c0f34ff2d659a417df3252280e3cc770ed2d43c5647f33420401000370a52c457a44e9025d647bdd1dbd4fa'
