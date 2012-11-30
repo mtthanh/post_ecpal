687040eb1379d0830f0d62edb110686b43aa513d
Thanh <mtthanh1986@gmail.com>
2012-11-30 10:11:25 +0700

    log ip address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def log(tag, json)
+    json["ip"] = request.remote_ip
     Fluent::Logger::FluentLogger.open(nil, :host=>Rails.application.config.fluent_host, :port=>Rails.application.config.fluent_port)
     Fluent::Logger.post(tag, json)
   end
